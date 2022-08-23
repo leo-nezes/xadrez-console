d8d95e7375f62fd2a929adf187c615af8d603938
Leonardo Menezes Libório <leonardo.liborio@niteo.com.br>
2022-08-22 23:21:15 -0300

    feat: Adicionando classes bases - Criando classes : Cor, Peca e Tabuleiro

---
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QuantidadeMovimento { get; protected set; }
+        public Tabuleiro Tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tabuleiro = tabuleiro;
+            QuantidadeMovimento = 0;
+        }
+    }
+}
