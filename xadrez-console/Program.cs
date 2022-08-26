0e614f892bf23a8ec0aa52e3946250ef13bbb7b0
Leonardo Menezes Liborio <leonardo.liborio@niteo.com.br>
2022-08-25 22:42:54 -0300

    feat: Leando a posição da peça no xadrez - Adicionando e ajustando nome de variável na classe PartidaDeXadrez - Exibindo tabuleiro enquanto a partida de xadrez for válida - Criando função para ler a posição da peça no tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,13 +8,45 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez posicao = new PosicaoXadrez('c', 7);
+            try
+            {
+                PartidaDeXadrez partida = new();
 
-            Console.WriteLine(posicao);
+                while (!partida.Terminada)
+                {
+                    Console.Clear();
 
-            Console.WriteLine(posicao.ToPosicao());
+                    Tela.ImprimirTabuleiro(partida.Tabuleiro);
 
-            Console.ReadLine();
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+
+                    partida.ExcecutarMovimento(origem, destino);
+                }
+
+                //tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                //tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                //tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 2));
+
+                //tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(3, 5));
+
+                Tela.ImprimirTabuleiro(partida.Tabuleiro);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //PosicaoXadrez posicao = new PosicaoXadrez('c', 7);
+
+            //Console.WriteLine(posicao);
+
+            //Console.WriteLine(posicao.ToPosicao());
+
+            //Console.ReadLine();
         }
     }
 }
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -5,41 +5,43 @@ namespace xadrez
 {
     internal class PartidaDeXadrez
     {
-        public Tabuleiro _tabuleiro { get; private set; }
+        public Tabuleiro Tabuleiro { get; private set; }
         private int _turno;
         private Cor _jogadorAtual;
+        public bool Terminada { get; private set; }
 
         public PartidaDeXadrez()
         {
-            _tabuleiro = new Tabuleiro(8, 8);
+            Tabuleiro = new Tabuleiro(8, 8);
             _turno = 1;
             _jogadorAtual = Cor.Branca;
+            Terminada = false;
             ColocarPecas();
         }
 
         public void ExcecutarMovimento(Posicao origem, Posicao destino)
         {
-            Peca peca = _tabuleiro.RetirarPeca(origem);
+            Peca peca = Tabuleiro.RetirarPeca(origem);
             peca.IncrementarQuantidadeMovimentos();
-            Peca pecaCapturada = _tabuleiro.RetirarPeca(destino);
-            _tabuleiro.ColocarPeca(peca, destino);
+            Peca pecaCapturada = Tabuleiro.RetirarPeca(destino);
+            Tabuleiro.ColocarPeca(peca, destino);
         }
 
         private void ColocarPecas()
         {
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Branca), new PosicaoXadrez('c', 1).ToPosicao());
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Branca), new PosicaoXadrez('c', 2).ToPosicao());
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Branca), new PosicaoXadrez('d', 2).ToPosicao());
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Branca), new PosicaoXadrez('e', 2).ToPosicao());
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Branca), new PosicaoXadrez('e', 1).ToPosicao());
-            _tabuleiro.ColocarPeca(new Rei(_tabuleiro, Cor.Branca), new PosicaoXadrez('d', 1).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Branca), new PosicaoXadrez('c', 1).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Branca), new PosicaoXadrez('c', 2).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Branca), new PosicaoXadrez('d', 2).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Branca), new PosicaoXadrez('e', 2).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Branca), new PosicaoXadrez('e', 1).ToPosicao());
+            Tabuleiro.ColocarPeca(new Rei(Tabuleiro, Cor.Branca), new PosicaoXadrez('d', 1).ToPosicao());
 
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Preta), new PosicaoXadrez('c', 7).ToPosicao());
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Preta), new PosicaoXadrez('c', 8).ToPosicao());
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Preta), new PosicaoXadrez('d', 7).ToPosicao());
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Preta), new PosicaoXadrez('e', 7).ToPosicao());
-            _tabuleiro.ColocarPeca(new Torre(_tabuleiro, Cor.Preta), new PosicaoXadrez('e', 8).ToPosicao());
-            _tabuleiro.ColocarPeca(new Rei(_tabuleiro, Cor.Preta), new PosicaoXadrez('d', 8).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Preta), new PosicaoXadrez('c', 7).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Preta), new PosicaoXadrez('c', 8).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Preta), new PosicaoXadrez('d', 7).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Preta), new PosicaoXadrez('e', 7).ToPosicao());
+            Tabuleiro.ColocarPeca(new Torre(Tabuleiro, Cor.Preta), new PosicaoXadrez('e', 8).ToPosicao());
+            Tabuleiro.ColocarPeca(new Rei(Tabuleiro, Cor.Preta), new PosicaoXadrez('d', 8).ToPosicao());
         }
      }
 }
